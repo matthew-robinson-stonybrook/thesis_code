1049e9eee1723762aa1439a101b54dcd878961d4
matthew-robinson-stonybrook <matthew.robinson@stonybrook.edu>
2021-10-31 11:21:03 -0400

    New Commit

---
+++ Robinson_Code_Git_Push.bash
@@ -0,0 +1,41 @@
+#!/bin/bash
+git_token=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
+
+echo "getting status of git"
+
+git status
+
+echo " "
+read -p "Would you like to add all files to git? (y/n) " add
+echo " "
+
+if [ $add == "y" ] 
+then
+   git add .
+   echo " "
+   echo "Getting updated status of git"
+   echo " "
+   git status
+   
+   echo " "
+   read -p "Would you like to commit all added files to git? (y/n) " commit
+   if [ $commit == "y" ] 
+   then
+      read -p "Commit Message: " msg
+      git commit -m "$msg"
+      echo " "
+      echo "Getting updated status of git"
+      echo " "
+      git status
+      echo " "
+      echo "! ! ! ! ! GIT USERNAME AND TOKEN FOR AUTHENTICATION ! ! ! ! !"
+      echo "! ! ! ! ! GIT TOKEN EXPIRED 1/22/2022 ! ! ! ! !"
+      echo "Git username: matthew-robinson-stonybrook"
+      echo "Git Token (for password): ${git_token}"
+      echo " "
+      
+      git push
+   else
+      echo "Fine be that way."
+   fi
+fi

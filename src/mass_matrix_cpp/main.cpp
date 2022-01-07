e25e6bf5b755618522e0a1d34f87259b13107397
matthew-robinson-stonybrook <matthew.robinson@stonybrook.edu>
2022-01-06 20:29:36 -0500

    "Created third mass matrix class and used method in Prof Chak lecture slides (calculating each mass matrix element one by one)"

--- src/mass_matrix_cpp/main.cpp
+++ src/mass_matrix_cpp/main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include "mass_matrix.h"
 #include "mass_matrix2.h"
+#include "mass_matrix3.h"
 #include "Baxter.h"
 #include "linalg.h"
 #include "transformation.h"
@@ -20,38 +21,29 @@ int main() {
    Baxter *baxter_ptr {nullptr};
    Baxter baxter;
    baxter_ptr = &baxter;
+   baxter_ptr->thetas = {pi/4, 0, pi/8, 0, pi/4, 0, 0};
    
-   //baxter_ptr->thetas = {pi/4, 0, pi/8, 0, pi/4, 0, 0};
-   baxter_ptr->thetas = {pi/4, 0, 0, 0, 0,  0, 0};
-    
-   Mass_Matrix mass(baxter_ptr);
-   mass.calculate_mass_matrix();
+   Baxter *baxter_ptr2 {nullptr};
+   Baxter baxter2;
+   baxter_ptr2 = &baxter2;
+   baxter_ptr2->thetas = {pi/4, 0, pi/8, 0, pi/4, 0, 0};
    
    Mass_Matrix2 mass2(baxter_ptr);
-   //mass2.calculate_mass_matrix();
-   mass2.calculate_screws();
-
+   mass2.calculate_mass_matrix();
+   Mass_Matrix3 mass3(baxter_ptr);
+   mass3.calculate_mass_matrix();
    
-   cout << "Mass1: " << endl;
-   cout << mass.link_oJacs.at(2) << endl;
-   cout << "Mass2 : " << endl;
-   cout << mass2.calculate_link_Jac(3) << endl;  
-    
-   Matrix<double,6,1> twist1_test {1, 3, 5, 7, 9, 11};
-   Matrix<double,6,1> twist2_test {2, 4, 6, 8, 10, 12};
-   Matrix<double,6,1> twist_product {g::lie_bracket(twist1_test, twist2_test)};
-   
-   cout << "Original Twist1: " << endl;
-   cout << twist1_test << endl;
-   cout << "Original Twist2: " << endl;
-   cout << twist2_test << endl;
-   cout << "Product " << endl;
-   cout << twist_product << endl; 
-   
-   MatrixXd m2_test = mass2.calculate_mass_matrix2();
-   cout << "Mass Matrix calc 2: " << endl;
-   cout << m2_test << endl;
+   MatrixXd mass_test = mass2.calculate_mass_matrix2();
    
    
+   cout << " " << endl;
+   cout << "Mass Matrix 2: " << endl;
+   cout << mass2.mass_matrix << endl;
+   cout << " " << endl;
+   cout << "Mass Matrix 3: " << endl;
+   cout << mass3.mass_matrix << endl;
+   cout << " " << endl;
+   
+  
    return 0;
 }
--- src/mass_matrix_cpp/mass_matrix3.h
+++ src/mass_matrix_cpp/mass_matrix3.h
@@ -1,5 +1,5 @@
-#ifndef _MASS_MATRIX2_H_
-#define _MASS_MATRIX2_H_
+#ifndef _MASS_MATRIX3_H_
+#define _MASS_MATRIX3_H_
 
 #include<iostream>
 #include<vector>
@@ -12,7 +12,7 @@
 using namespace std;
 using namespace Eigen;
 
-class Mass_Matrix2 {
+class Mass_Matrix3 {
    public:
       Baxter *baxter;
       int joints;
@@ -32,22 +32,19 @@ class Mass_Matrix2 {
       vector <MatrixXd> actuator_oJacs;
       vector <Matrix3d> actuator_rots;   
 
-      Mass_Matrix2(Baxter*);
+      Mass_Matrix3(Baxter*);
       
       void calculate_twist_coords();
       Matrix<double, 6, 6> calculate_generalized_mass(int mass, Matrix3d inertia);
       Matrix<double, 6, 6> calculate_adjusted_gmass(Matrix<double, 6, 6> gmass, int link);
       Matrix<double, 6, 6> calculate_adjoint_ij(int i, int j);
       
-      MatrixXd calculate_link_Jac(int);
-      
       //MatrixXd calculate_actuator_Jac();
       void calculate_mass_matrix();
-      MatrixXd calculate_mass_matrix2();
 
 };
 
-Mass_Matrix2::Mass_Matrix2(Baxter* manip) : baxter{manip} {
+Mass_Matrix3::Mass_Matrix3(Baxter* manip) : baxter{manip} {
    joints = baxter->joints;
    axis_joints = baxter->axis_joints;
    q_joints = baxter->q_joints;
@@ -55,7 +52,7 @@ Mass_Matrix2::Mass_Matrix2(Baxter* manip) : baxter{manip} {
    
 }
 
-void Mass_Matrix2::calculate_twist_coords(){
+void Mass_Matrix3::calculate_twist_coords(){
    twist_coords.resize(joints, 6);
    Vector3d cross;
    Vector3d uj;
@@ -77,7 +74,7 @@ void Mass_Matrix2::calculate_twist_coords(){
    }
 }
 
-Matrix<double, 6, 6> Mass_Matrix2::calculate_generalized_mass(int mass, Matrix3d inertia) {
+Matrix<double, 6, 6> Mass_Matrix3::calculate_generalized_mass(int mass, Matrix3d inertia) {
    Matrix<double, 6, 6> gen_mass;
    
    for(int row{0}; row < 6; row++) {
@@ -99,7 +96,7 @@ Matrix<double, 6, 6> Mass_Matrix2::calculate_generalized_mass(int mass, Matrix3d
    return gen_mass;
 }
 
-Matrix<double, 6, 6> Mass_Matrix2::calculate_adjusted_gmass(Matrix<double, 6, 6> gmass, int link) {
+Matrix<double, 6, 6> Mass_Matrix3::calculate_adjusted_gmass(Matrix<double, 6, 6> gmass, int link) {
    Matrix4d gsli0 = linalg::eye4;
    for(int joint{0}; joint < link; joint++) {
       double theta = baxter->thetas(joint);
@@ -120,7 +117,7 @@ Matrix<double, 6, 6> Mass_Matrix2::calculate_adjusted_gmass(Matrix<double, 6, 6>
    return adjusted_gmass;
 }
 
-Matrix<double, 6, 6> Mass_Matrix2::calculate_adjoint_ij(int i, int j) {
+Matrix<double, 6, 6> Mass_Matrix3::calculate_adjoint_ij(int i, int j) {
    Matrix<double, 6, 6> adjoint;
    Matrix4d g {linalg::eye4};
    
@@ -145,84 +142,19 @@ Matrix<double, 6, 6> Mass_Matrix2::calculate_adjoint_ij(int i, int j) {
    return adjoint;
 }
 
-MatrixXd Mass_Matrix2::calculate_link_Jac(int link) {
-   MatrixXd jac;
-   jac.resize(6,joints);
-   
-   
-   // Zero out matrix
-   for (int i{0}; i < 6; i++) {
-      for (int j{0}; j < joints; j++) {
-         jac(i, j) = 0;
-      }
-   }
-   
-   
-   MatrixXd p_link {baxter->p_links.row(link-1)};
-   Matrix4d gsli;
-   Matrix<double, 6, 1> ej_dagger;
-   Matrix4d gj_hat {linalg::eye4};
-   
-   for(int col{0}; col < link; col++) {     
-      gsli = linalg::eye4;
-      gsli(0, 3) = p_link(0);
-      gsli(1, 3) = p_link(1);
-      gsli(2, 3) = p_link(2);
-      
-      for(int t{col}; t < link; t++) {
-         Matrix4d gi = g::twist(twist_coords.row(t), baxter->thetas(t));
-         gj_hat *= gi;
-      }
-      
-      gj_hat *= gsli;
-      Matrix4d gj_hat_inv = gj_hat.inverse();
-      ej_dagger = g::adjoint(gj_hat_inv) * twist_coords.row(col).transpose();
-      
-      for(int row{0}; row <= 5; row++) {
-         jac(row,col) = ej_dagger(row);
-      }
-   }
-   
-   return jac;
-}
-
-void Mass_Matrix2::calculate_mass_matrix() {
-   calculate_twist_coords();
-
-   // Zero out matrix
-   for(int row{0}; row < joints; row++) {
-      for(int col{0}; col < joints; col++) {
-         mass_matrix(row,col) = 0;
-      }
-   }
-   
-   Matrix<double, 6, 6> gen_mass;
-   MatrixXd jacobian;
-   
-   // Add link terms
-   for(int link{1}; link <= joints; link++) {
-      gen_mass = calculate_generalized_mass(baxter->mls.at(link-1), baxter->Ils.at(link-1));
-      jacobian = calculate_link_Jac(link);
-      mass_matrix += jacobian.transpose() * gen_mass * jacobian;
-   }
-}
-
-
-MatrixXd Mass_Matrix2::calculate_mass_matrix2() {
-   cout << "M2 " << endl;
+void Mass_Matrix3::calculate_mass_matrix() {
+   cout << "M3 " << endl;
    
    calculate_twist_coords();
    
    const int js {baxter->joints};
-   MatrixXd mass_matrix;
-   mass_matrix.resize(js,js);
    
    for(int row{0}; row < js; row++) {
       for(int col{0}; col < js; col++) {
          mass_matrix(row,col) = 0;
       }
    }
-    
+   
    int m;
    Matrix3d I;
    MatrixXd p_link;
@@ -253,9 +185,6 @@ MatrixXd Mass_Matrix2::calculate_mass_matrix2() {
          mass_matrix(row,col) = val;
       }
    }
-   
-   return mass_matrix;
-   
 }
 
 #endif
